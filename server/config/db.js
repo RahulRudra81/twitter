9048399cebe7374640316e4287df9090a1372357
rahulrudra81 <rahulrudra81@gmail.com>
2024-04-19 15:50:43 +0530

    front end most part done

--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const dbStart = async (url) => {
-  await mongoose.connect(url);
-  console.log("Connected to database");
-};
-
-export default dbStart;
--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import { registerUser } from "../controllers/auth";
-import { authUser } from "../controllers/auth";
-
-const router = express.Router();
-
-router.route("/register").post(registerUser);
-router.route("/auth").post(validateToken, authUser);
-
-module.exports = router;
